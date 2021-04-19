e5bec27b570c0e93b3e82a7bf17e9e92bd45b0ab
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-04-19 10:21:30 -0300

    fix: added UserJWTDTO properties

--- src/CommonsModule/dto/user-jwt.dto.ts
+++ src/CommonsModule/dto/user-jwt.dto.ts
@@ -1,15 +1,46 @@
 import { Expose, Type } from 'class-transformer';
-import { IsNotEmpty } from 'class-validator';
-import { UserDTO } from '../../UserModule/dto/user.dto';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
-export class UserJWTDTO extends UserDTO {
+export class UserJWTDTO {
+  @IsNotEmpty()
+  @IsString()
   @Expose()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Expose()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Expose()
+  username: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  @Expose()
+  enabled: boolean;
+
+  @Expose()
+  @IsNotEmpty()
   role: RoleJWTDTO;
+
+  @IsString()
+  @Expose()
+  phone?: string;
+
+  @IsString()
+  @Expose()
+  email?: string;
 }
 
 export class RoleJWTDTO {
   @Expose()
   id: string;
+
+  @Expose()
+  name: string;
 
   @Type(() => PolicyJWTDTO)
   @IsNotEmpty()
